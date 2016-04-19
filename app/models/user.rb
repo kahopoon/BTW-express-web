9a3008ffb8bf018f8696fe996a5985b6929b0ccf
J.CHO <jchobanana@gmail.com>
2016-04-20 01:56:50 +0800

    add auth_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+
+
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+   # self.authentication_token = SecureRandom.hex(16)
+   self.authentication_token = Devise.friendly_token
+  end
+
+  def get_fb_data
+   j = RestClient.get "https://graph.facebook.com/v2.5/me", :params => { :access_token => self.fb_token, :fields => "id,name,email,picture" }
+   JSON.parse(j)
+  end
+
   def self.from_omniauth(auth)
       # Case 1: Find existing user by facebook uid
       user = User.find_by_fb_uid( auth.uid )
