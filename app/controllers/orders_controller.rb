085a818b439d241752e61ff7ac8ad7d993f99de5
J.CHO <jchobanana@gmail.com>
2016-04-21 17:46:29 +0800

    pickup/delivery controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,12 +8,16 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
+    @order.build_detail
+    @order.detail.build_pickup
+    @order.detail.build_delivery
   end
 
 private
 
   def order_params
-    params.require(:order).permit( :owner_id )
+    params.require(:order).permit( :owner_id, :owner_id, :courier_id, :status )
   end
 
 end
+
