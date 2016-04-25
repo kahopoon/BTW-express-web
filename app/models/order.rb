1fdd18502e7618acad2fde98e04b0b3ef1069d04
J.CHO <jchobanana@gmail.com>
2016-04-26 01:58:05 +0800

    order_create revision & nav fixed & show page fixed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   has_one :detail
   belongs_to :user
-
+  accepts_nested_attributes_for :detail
 
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   do_not_validate_attachment_file_type :photo, content_type: /\Aimage\/.*\Z/
