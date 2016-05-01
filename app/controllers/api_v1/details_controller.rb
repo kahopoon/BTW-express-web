89ec09872cf3fb92e023546892aa8db5bd81d846
ching-fu <gavaniyu@hotmail.com>
2016-05-01 17:19:05 +0800

    Ajax to update #deatil-description on views/orders/index.html.erb

--- app/controllers/api_v1/details_controller.rb
+++ app/controllers/api_v1/details_controller.rb
@@ -3,5 +3,8 @@ class ApiV1::DetailsController < ApiController
   def index
     render :json => { :message => "hugo is so hot" }
   end
-
+  def show
+  	o_id=params[:o_id].split('o').last
+  	@detail=Order.find(o_id).detail
+  end
 end
---
+++ app/views/api_v1/details/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.detail_id @detail.id
+json.order_id @detail.order.id
+json.description simple_format(@detail.description)
