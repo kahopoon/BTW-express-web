8b001dc8ea453c5646c1cfb051d76dd110ceb80b
J.CHO <jchobanana@gmail.com>
2016-05-05 17:19:45 +0800

    fb_pic api, add column

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   end
 
   def self.get_fb_data(access_token)
-    res = RestClient.get "https://graph.facebook.com/v2.4/me",  { :params => { :access_token => access_token } }
+    res = RestClient.get "https://graph.facebook.com/v2.4/me",  { :params => { :fields => "email,name, picture.type(large)", :access_token => access_token } }
 
     if res.code == 200
     JSON.parse( res.to_str)
@@ -55,6 +55,40 @@ class User < ActiveRecord::Base
       user.save!
       return user
     end
+
+    def self.from_omniauth_api(auth)
+        # Case 1: Find existing user by facebook uid
+        user = User.find_by_fb_uid( auth.uid )
+        if user
+           user.fb_token = auth.credentials.token
+           #user.fb_raw_data = auth
+           user.save!
+          return user
+        end
+
+        # Case 2: Find existing user by email
+        existing_user = User.find_by_email( auth.info.email )
+        if existing_user
+          existing_user.fb_uid = auth.uid
+          existing_user.fb_token = auth.credentials.token
+          #existing_user.fb_raw_data = auth
+          existing_user.save!
+          return existing_user
+        end
+
+        # Case 3: Create new password
+        user = User.new
+        byebug
+        user.fb_uid = auth.uid
+        user.fb_pic = auth.info.picture.data.url
+        user.fb_token = auth.credentials.token
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        #user.fb_raw_data = auth
+        user.save!
+        return user
+      end
+
     def user_approve(id,token)
       #if id & token match, return user
     end
