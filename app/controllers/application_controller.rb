1825072f98c2e9887c174e2f67b4bfa12a9ca72e
J.CHO <jchobanana@gmail.com>
2016-05-06 21:10:07 +0800

    added fb_pic in show/edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-
-   if current_user.introduction == nil  #之後換成簡介
-     edit_user_path(current_user) #之後把要填資料的path放這
-   else
-     request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-   end
-end
+    if current_user.introduction.nil? # 之後換成簡介
+      edit_user_path(current_user) # 之後把要填資料的path放這
+    else
+      request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -242,7 +242,7 @@ Devise.setup do |config|
   fb_config = Rails.application.config_for(:facebook)
 
   config.omniauth :facebook, fb_config["app_id"], fb_config["secret"],
-                  :scope => 'public_profile,email', :info_fields => 'email,name'
+                  :scope => 'public_profile,email', :info_fields => 'email,name', :image_size => 'large'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
